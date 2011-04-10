f25db3376d68e00a56da9e58ab297efa10bc884c
Stan Angeloff <stanimir@psp-webtech.co.uk>
2011-04-10 19:10:35 +0300

    Script to SSH to my newly installed Arch (replacing Cygwin).

---
+++ ssh_arch_linux_2011.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+VALUE=$( "`/usr/bin/cygpath -u "$VBOX_INSTALL_PATH/VBoxManage.exe"`" guestproperty get "Arch Linux 2011" "/VirtualBox/GuestInfo/Net/0/V4/IP" )
+if [[ $VALUE == Value* ]]; then
+  IP=$( echo "$VALUE" | /usr/bin/cut -c8-99 | /usr/bin/sed 's/$//' )
+else
+  read -e -p 'IP: ' IP
+fi
+
+/usr/bin/ssh $IP
