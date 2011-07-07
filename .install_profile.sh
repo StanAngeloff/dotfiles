01296e0c49dbb33365eac4956a73690a91dbc348
${name} <${email}>
2011-07-07 14:23:44 +0300

    Add install script to collect required information and update template files.

---
+++ .install_profile.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+VALUES=(
+  name 'Name'
+  email 'E-mail address'
+  username 'Github name'
+  token 'Github token'
+)
+TEMPLATES=( .gitconfig )
+
+INSTALL_PATH="$HOME/.install_values"
+INSTALL_VALUES=''
+
+SED_SCRIPT='sed '
+
+[ -f "$INSTALL_PATH" ] && source "$INSTALL_PATH"
+
+for (( i = 0 ; i < ${#VALUES[@]} ; i += 2 )); do
+  NAME="${VALUES[$i]}"
+  DESCRIPTION="${VALUES[$i + 1]}"
+  VALUE="INSTALL_$NAME"
+  PREVIOUS="${!VALUE}"
+  echo -n "Please enter your $DESCRIPTION [$PREVIOUS]: "
+  read "$VALUE"
+  [ -z "${!VALUE}" ] && eval $VALUE=\"$PREVIOUS\"
+  INSTALL_VALUES="$INSTALL_VALUES"$'\n'"$VALUE='${!VALUE}'"
+  SED_SCRIPT="$SED_SCRIPT -e 's/''$PREVIOUS'' # \${$NAME}\|\${$NAME}/'""'${!VALUE}'""' # \${$NAME}/g'"
+done
+
+echo "$INSTALL_VALUES" > "$INSTALL_PATH"
+
+for file in $TEMPLATES; do
+  eval "$SED_SCRIPT" -i "$file"
+done
