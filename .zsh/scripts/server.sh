a42a2cbd7e1524c5f2ac5f1595a0888d1c0f8285
Stan Angeloff <stanimir@psp-webtech.co.uk>
2012-07-16 15:03:35 +0300

    Zsh: use 2 spaces per indent, #nfc.

--- .zsh/scripts/server.sh
+++ .zsh/scripts/server.sh
@@ -1,7 +1,7 @@
 # Start an HTTP server from a directory, optionally specifying the port (default 3030).
 
 function server() {
-    local port="${1:-3030}"
-    x-www-browser "http://127.0.0.1:${port}/"
-    python -m 'SimpleHTTPServer' "$port"
+  local port="${1:-3030}"
+  x-www-browser "http://127.0.0.1:${port}/"
+  python -m 'SimpleHTTPServer' "$port"
 }
