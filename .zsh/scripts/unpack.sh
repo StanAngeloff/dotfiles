da411979631a465a36ecdd4ed0512b1021e660dc
Stan Angeloff <stanimir@psp-webtech.co.uk>
2012-08-05 15:26:56 +0300

    Zsh: +unpack function.
    Quick and worry-free way to unpack archives.

---
+++ .zsh/scripts/unpack.sh
@@ -0,0 +1,26 @@
+# Idea from https://github.com/yomi322/config/blob/master/dot.zshrc
+
+function unpack() {
+  if [ $# -lt 1 ]; then
+    echo 'Usage: '"$0"' FILE [DIRECTORY]' >/dev/stderr
+    return 1
+  fi
+  local file
+  local directory
+  local command
+  file="$( readlink -f "$1" )"
+  directory="${2:-.}"
+  case "$file" in
+    *.tar.gz  | *.tgz  ) command='tar -zxvf' ;;
+    *.tar.bz2 | *.tbz2 ) command='tar -jxvf' ;;
+    *.tar              ) command='tar -xvf'  ;;
+    *.zip              ) command='unzip'     ;;
+    *.gzip  | *.gz     ) command='gunzip'    ;;
+    *.bzip2 | *.bz2    ) command='bunzip2'   ;;
+    * )
+      echo "$0: Unsupported file format: $file" >/dev/stderr ;
+      return 2
+      ;;
+  esac
+  ( cd "$directory" && eval $command "\$file" )
+}
