f364a13a9e9989660af1d45a8681a202855fd945
Stan Angeloff <stanimir@psp-webtech.co.uk>
2024-01-23 12:22:13 +0200

    vim: organize plugins by alphabetical order.

--- .vim/lua/core/plugin_config/init.lua
+++ .vim/lua/core/plugin_config/init.lua
@@ -1,6 +1,7 @@
 require('core.plugin_config.autopairs')
+require('core.plugin_config.lsp_config')
+require('core.plugin_config.lualine')
+require('core.plugin_config.treesitter')
+
 -- require('core.plugin_config.gitsigns')
 -- require('core.plugin_config.indent_blankline')
-require('core.plugin_config.lualine')
-require('core.plugin_config.lsp_config')
-require('core.plugin_config.treesitter')
